2b47b7c6f8ff41ebe6776fb8e0cf509b98fa89bf
Rcuhi <rappinngu2006@gmail.com>
2023-10-30 21:32:46 +0900

    行列クラスのバグ修正

--- src/matrix4x4.js
+++ src/matrix4x4.js
@@ -1,5 +1,4 @@
-import { Vector3 } from "./vector3";
-import { Mathf } from "./math";
+import { Mathf } from "./math.js";
 
 export class Matrix4x4 {
     //m11 m12 m13 m14
@@ -29,7 +28,7 @@ export class Matrix4x4 {
         this.m44 = m44;
     }
 
-    get identify() {
+    static get identify() {
         return new Matrix4x4(
             1, 0, 0, 0,
             0, 1, 0, 0,
@@ -83,33 +82,33 @@ export class Matrix4x4 {
         return mat;
     }
 
-    multiply() {
-        const matrix = Matrix4x4.identify();
+    multiply(m1) {
+        const matrix = Matrix4x4.identify;
 
-        matrix.m11 = m1.m11 * m2.m11 + m1.m12 * m2.m21 + m1.m13 * m2.m31 + m1.m14 * m2.m41;
-        matrix.m21 = m1.m21 * m2.m11 + m1.m22 * m2.m21 + m1.m23 * m2.m31 + m1.m24 * m2.m41;
-        matrix.m31 = m1.m31 * m2.m11 + m1.m32 * m2.m21 + m1.m33 * m2.m31 + m1.m34 * m2.m41;
-        matrix.m41 = m1.m41 * m2.m11 + m1.m42 * m2.m21 + m1.m43 * m2.m31 + m1.m44 * m2.m41;
+        matrix.m11 = this.m11 * m1.m11 + this.m12 * m1.m21 + this.m13 * m1.m31 + this.m14 * m1.m41;
+        matrix.m21 = this.m21 * m1.m11 + this.m22 * m1.m21 + this.m23 * m1.m31 + this.m24 * m1.m41;
+        matrix.m31 = this.m31 * m1.m11 + this.m32 * m1.m21 + this.m33 * m1.m31 + this.m34 * m1.m41;
+        matrix.m41 = this.m41 * m1.m11 + this.m42 * m1.m21 + this.m43 * m1.m31 + this.m44 * m1.m41;
 
-        matrix.m12 = m1.m11 * m2.m12 + m1.m12 * m2.m22 + m1.m13 * m2.m32 + m1.m14 * m2.m42;
-        matrix.m22 = m1.m21 * m2.m12 + m1.m22 * m2.m22 + m1.m23 * m2.m32 + m1.m24 * m2.m42;
-        matrix.m32 = m1.m31 * m2.m12 + m1.m32 * m2.m22 + m1.m33 * m2.m32 + m1.m34 * m2.m42;
-        matrix.m42 = m1.m41 * m2.m12 + m1.m42 * m2.m22 + m1.m43 * m2.m32 + m1.m44 * m2.m42;
+        matrix.m12 = this.m11 * m1.m12 + this.m12 * m1.m22 + this.m13 * m1.m32 + this.m14 * m1.m42;
+        matrix.m22 = this.m21 * m1.m12 + this.m22 * m1.m22 + this.m23 * m1.m32 + this.m24 * m1.m42;
+        matrix.m32 = this.m31 * m1.m12 + this.m32 * m1.m22 + this.m33 * m1.m32 + this.m34 * m1.m42;
+        matrix.m42 = this.m41 * m1.m12 + this.m42 * m1.m22 + this.m43 * m1.m32 + this.m44 * m1.m42;
 
-        matrix.m13 = m1.m11 * m2.m13 + m1.m12 * m2.m23 + m1.m13 * m2.m33 + m1.m14 * m2.m43;
-        matrix.m23 = m1.m21 * m2.m13 + m1.m22 * m2.m23 + m1.m23 * m2.m33 + m1.m24 * m2.m43;
-        matrix.m33 = m1.m31 * m2.m13 + m1.m32 * m2.m23 + m1.m33 * m2.m33 + m1.m34 * m2.m43;
-        matrix.m43 = m1.m41 * m2.m13 + m1.m42 * m2.m23 + m1.m43 * m2.m33 + m1.m44 * m2.m43;
+        matrix.m13 = this.m11 * m1.m13 + this.m12 * m1.m23 + this.m13 * m1.m33 + this.m14 * m1.m43;
+        matrix.m23 = this.m21 * m1.m13 + this.m22 * m1.m23 + this.m23 * m1.m33 + this.m24 * m1.m43;
+        matrix.m33 = this.m31 * m1.m13 + this.m32 * m1.m23 + this.m33 * m1.m33 + this.m34 * m1.m43;
+        matrix.m43 = this.m41 * m1.m13 + this.m42 * m1.m23 + this.m43 * m1.m33 + this.m44 * m1.m43;
 
-        matrix.m14 = m1.m11 * m2.m14 + m1.m12 * m2.m24 + m1.m13 * m2.m34 + m1.m14 * m2.m44;
-        matrix.m24 = m1.m21 * m2.m14 + m1.m22 * m2.m24 + m1.m23 * m2.m34 + m1.m24 * m2.m44;
-        matrix.m34 = m1.m31 * m2.m14 + m1.m32 * m2.m24 + m1.m33 * m2.m34 + m1.m34 * m2.m44;
-        matrix.m44 = m1.m41 * m2.m14 + m1.m42 * m2.m24 + m1.m43 * m2.m34 + m1.m44 * m2.m44;
+        matrix.m14 = this.m11 * m1.m14 + this.m12 * m1.m24 + this.m13 * m1.m34 + this.m14 * m1.m44;
+        matrix.m24 = this.m21 * m1.m14 + this.m22 * m1.m24 + this.m23 * m1.m34 + this.m24 * m1.m44;
+        matrix.m34 = this.m31 * m1.m14 + this.m32 * m1.m24 + this.m33 * m1.m34 + this.m34 * m1.m44;
+        matrix.m44 = this.m41 * m1.m14 + this.m42 * m1.m24 + this.m43 * m1.m34 + this.m44 * m1.m44;
 
         return matrix;
     }
 
     toString() {
-        return `((${m11}, ${m12}, ${m13}, ${m14}), (${m21}, ${m22}, ${m23}, ${m24}), (${m31}, ${m32}, ${m33}, ${m34}), (${m41}, ${m42}, ${m43}, ${m44})`;
+        return `((${this.m11}, ${this.m12}, ${this.m13}, ${this.m14}), (${this.m21}, ${this.m22}, ${this.m23}, ${this.m24}), (${this.m31}, ${this.m32}, ${this.m33}, ${this.m34}), (${this.m41}, ${this.m42}, ${this.m43}, ${this.m44})`;
     }
 }
